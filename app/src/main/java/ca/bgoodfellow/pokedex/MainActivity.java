852198fa2d493aacee4e79d6312e7ce8b660003a
bgood92 <goodfellow.brandon@gmail.com>
2018-03-14 15:49:06 -0500

    Added a functionality where a user can click on an item in MainActivity and an intent object is passed to PokemonActivity, carrying the data associated with the item clicked, and displays it in the new activity.

--- app/src/main/java/ca/bgoodfellow/pokedex/MainActivity.java
+++ app/src/main/java/ca/bgoodfellow/pokedex/MainActivity.java
@@ -1,6 +1,7 @@
 package ca.bgoodfellow.pokedex;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -33,13 +35,50 @@ public class MainActivity extends AppCompatActivity {
         pokemonAdapter = new PokemonAdapter(this, R.layout.list_item, pokemonList);
 
         lvPokemon.setAdapter(pokemonAdapter);
+
+        lvPokemon.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(view.getContext(), PokemonActivity.class);
+
+                TextView entry = (TextView) view.findViewById(R.id.tvEntry);
+                TextView name = (TextView) view.findViewById(R.id.tvName);
+                TextView type = (TextView) view.findViewById(R.id.tvType);
+                TextView gen = (TextView) view.findViewById(R.id.tvGen);
+                TextView hp = (TextView) view.findViewById(R.id.tvHP);
+                TextView atk = (TextView) view.findViewById(R.id.tvAtk);
+                TextView def = (TextView) view.findViewById(R.id.tvDef);
+                TextView spAtk = (TextView) view.findViewById(R.id.tvSpAtk);
+                TextView spDef = (TextView) view.findViewById(R.id.tvSpDef);
+                TextView spe = (TextView) view.findViewById(R.id.tvSpe);
+
+                intent.putExtra("entry", entry.getText().toString());
+                intent.putExtra("name", name.getText().toString());
+                intent.putExtra("type", type.getText().toString());
+                intent.putExtra("gen", gen.getText().toString());
+                intent.putExtra("hp", hp.getText().toString());
+                intent.putExtra("atk", atk.getText().toString());
+                intent.putExtra("def", def.getText().toString());
+                intent.putExtra("spAtk", spAtk.getText().toString());
+                intent.putExtra("spDef", spDef.getText().toString());
+                intent.putExtra("spe", spe.getText().toString());
+
+                startActivity(intent);
+            }
+        });
     }
 
     private void generatePokemon(ArrayList<Pokemon> pokemonList)
     {
         pokemonList.add(new Pokemon("Bulbasaur", "Grass", "Poison", 1, 1, 11,11,11,11,11,11));
-        pokemonList.add(new Pokemon("Charmander", "Fire", null, 1, 1, 11,11,11,11,11,11));
-        pokemonList.add(new Pokemon("Bulbasaur", "Grass", "Poison", 1, 1, 11,11,11,11,11,11));
+        pokemonList.add(new Pokemon("Ivysaur", "Grass", "Poison", 2, 1, 11,11,11,11,11,11));
+        pokemonList.add(new Pokemon("Veuasaur", "Grass", "Poison", 3, 1, 11,11,11,11,11,11));
+        pokemonList.add(new Pokemon("Charmander", "Fire", null, 4, 1, 11,11,11,11,11,11));
+        pokemonList.add(new Pokemon("Charmeleon", "Fire", null, 5, 1, 11,11,11,11,11,11));
+        pokemonList.add(new Pokemon("Charizard", "Fire", "Flying", 6, 1, 11,11,11,11,11,11));
+        pokemonList.add(new Pokemon("Squirtle", "Water", null, 7, 1, 11,11,11,11,11,11));
+        pokemonList.add(new Pokemon("Wartortle", "Water", null, 8, 1, 11,11,11,11,11,11));
+        pokemonList.add(new Pokemon("Blastoise", "Water", null, 9, 1, 11,11,11,11,11,11));
     }
 
     //!-------------------------Inner Classes-----------------------------!
@@ -63,11 +102,48 @@ public class MainActivity extends AppCompatActivity {
             if (p != null) {
                 TextView entry = (TextView) v.findViewById(R.id.tvEntry);
                 TextView name = (TextView) v.findViewById(R.id.tvName);
+                TextView type = (TextView) v.findViewById(R.id.tvType);
+                TextView gen = (TextView) v.findViewById(R.id.tvGen);
+                TextView hp = (TextView) v.findViewById(R.id.tvHP);
+                TextView atk = (TextView) v.findViewById(R.id.tvAtk);
+                TextView def = (TextView) v.findViewById(R.id.tvDef);
+                TextView spAtk = (TextView) v.findViewById(R.id.tvSpAtk);
+                TextView spDef = (TextView) v.findViewById(R.id.tvSpDef);
+                TextView spe = (TextView) v.findViewById(R.id.tvSpe);
                 if (entry != null) {
                     entry.setText(String.valueOf(p.getEntry()));
                 }
                 if (name != null) {
                     name.setText(p.getName());
+                }
+                if (type != null) {
+                    if (p.getSecondType() != null && p.getSecondType() != "") {
+                        type.setText(p.getFirstType() + ", " + p.getSecondType());
+                    }
+                    else {
+                        type.setText(p.getFirstType());
+                    }
+                }
+                if (gen != null) {
+                    gen.setText(String.valueOf(p.getGeneration()));
+                }
+                if (hp != null) {
+                    hp.setText(String.valueOf(p.getHp()));
+                }
+                if (atk != null) {
+                    atk.setText(String.valueOf(p.getAtk()));
+                }
+                if (def != null) {
+                    def.setText(String.valueOf(p.getDef()));
+                }
+                if (spAtk != null) {
+                    spAtk.setText(String.valueOf(p.getSpAtk()));
+                }
+                if (spDef != null) {
+                    spDef.setText(String.valueOf(p.getSpDef()));
+                }
+                if (spe != null) {
+                    spe.setText(String.valueOf(p.getSpe()));
                 }
             }
             return v;
@@ -94,47 +170,47 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public String getName() {
-            return name;
+            return this.name;
         }
 
         public String getFirstType() {
-            return firstType;
+            return this.firstType;
         }
 
         public String getSecondType() {
-            return secondType;
+            return this.secondType;
         }
 
         public int getEntry() {
-            return entry;
+            return this.entry;
         }
 
         public int getGeneration() {
-            return generation;
+            return this.generation;
         }
 
         public int getHp() {
-            return hp;
+            return this.hp;
         }
 
         public int getAtk() {
-            return atk;
+            return this.atk;
         }
 
         public int getDef() {
-            return def;
+            return this.def;
         }
 
         public int getSpAtk() {
-            return spAtk;
+            return this.spAtk;
         }
 
         public int getSpDef() {
-            return spDef;
+            return this.spDef;
         }
 
         public int getSpe() {
-            return spe;
+            return this.spe;
         }
     }
 }
--- app/src/main/java/ca/bgoodfellow/pokedex/PokemonActivity.java
+++ app/src/main/java/ca/bgoodfellow/pokedex/PokemonActivity.java
@@ -2,12 +2,54 @@ package ca.bgoodfellow.pokedex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PokemonActivity extends AppCompatActivity {
+    private TextView tvEntryResult, tvNameResult, tvTypeResult, tvGenResult,
+                    tvHPResult, tvAtkResult, tvDefResult,
+                    tvSpAtkResult, tvSpDefResult, tvSpeResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokemon);
+
+        tvEntryResult = findViewById(R.id.tvEntryResult);
+        tvNameResult = findViewById(R.id.tvNameResult);
+        tvTypeResult = findViewById(R.id.tvTypeResult);
+        tvGenResult = findViewById(R.id.tvGenResult);
+        tvHPResult = findViewById(R.id.tvHPResult);
+        tvAtkResult = findViewById(R.id.tvAtkResult);
+        tvDefResult = findViewById(R.id.tvDefResult);
+        tvSpAtkResult = findViewById(R.id.tvSpAtkResult);
+        tvSpDefResult = findViewById(R.id.tvSpDefResult);
+        tvSpeResult = findViewById(R.id.tvSpeResult);
+
+        String entry = getIntent().getStringExtra("entry");
+        String name = getIntent().getStringExtra("name");
+        String type = getIntent().getStringExtra("type");
+        String gen = getIntent().getStringExtra("gen");
+        String hp = getIntent().getStringExtra("hp");
+        String atk = getIntent().getStringExtra("atk");
+        String def = getIntent().getStringExtra("def");
+        String spAtk = getIntent().getStringExtra("spAtk");
+        String spDef = getIntent().getStringExtra("spDef");
+        String spe = getIntent().getStringExtra("spe");
+
+        tvEntryResult.setText("#" + entry);
+        tvNameResult.setText(name);
+        tvTypeResult.setText(type);
+        tvGenResult.setText(gen);
+        tvHPResult.setText(hp);
+        tvAtkResult.setText(atk);
+        tvDefResult.setText(def);
+        tvSpAtkResult.setText(spAtk);
+        tvSpDefResult.setText(spDef);
+        tvSpeResult.setText(spe);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
     }
 }
